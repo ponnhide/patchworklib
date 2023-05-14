23b478377344bf50ec86524f72916a99bbd1197c
ponnhide <morityun@sfc.keio.ac.jp>
2023-05-15 00:34:33 +0900

    Updated to version 0.6.0

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 ]
 
